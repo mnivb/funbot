a5d496a2447dfc6a545544268ff364a965b19738
mnivb <41504414+mnivb@users.noreply.github.com>
2020-09-30 18:52:54 +0530

    Update funbot.py

--- funbot.py
+++ funbot.py
@@ -29,8 +29,8 @@ def handle(msg):
     print(link)
     bot.sendPhoto(chat_id,link)
     bot.sendMessage(chat_id,'ok next')
-    context.bot.sendPhoto(chat_id, photo="https://t.me/addstickers/FRIDAY_653340100_Pack", caption="avna yaar uu")
-
+    #context.bot.sendPhoto(chat_id, photo="https://t.me/addstickers/FRIDAY_653340100_Pack", caption="avna yaar uu")
+    bot.sendPhoto(chat_id,'https://t.me/addstickers/FRIDAY_653340100_Pack')
 
 
 bot = telepot.Bot('914559098:AAGDM825JY2C-lmogTwnXx-kTpQ7VR1tN48')
